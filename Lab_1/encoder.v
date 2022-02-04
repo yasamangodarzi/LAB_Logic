ad73af7877011eb3d14be60a2073de63efc400cc
yasaman godarzi <78849693+yasamangodarzi@users.noreply.github.com>
2022-02-04 23:33:34 +0330

    Add files via upload

---
+++ Lab_1/encoder.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:02:44 11/07/2021 
+// Design Name: 
+// Module Name:    encoder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module encoder(
+    input I3,
+    input D1,
+    input D2,
+    input D3,
+    output Q0,
+    output Q1
+    );
+	 
+assign Q1 = D3 + D2;
+assign Q0 = D3 + ((~D2)&D1);
+
+endmodule
