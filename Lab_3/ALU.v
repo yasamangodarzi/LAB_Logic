289a436818a6efc505705a363e830edededb560e
yasaman godarzi <78849693+yasamangodarzi@users.noreply.github.com>
2022-02-04 23:58:09 +0330

    Add files via upload

---
+++ Lab_3/ALU.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    04:15:22 11/30/2021 
+// Design Name: 
+// Module Name:    ALU 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ALU (output [3:0] out, input [1:0] A,input [1:0] B, input [1:0]S );
+
+ 
+	
+	assign out = S[1] ? (S[0] ? A * B : (A + B) ) : (S[0] ? {2'b00,~(A & B)} : {2'b00,~A});
+	
+endmodule
+
+ 
